f248da8b35974b9cd311083a54c288eb19aba094
KegsZooL <danil.chistyakov.2015@inbox.ru>
2025-03-21 17:08:13 +0300

    Added almost basic functionality

--- echo/urls.py
+++ echo/urls.py
@@ -12,4 +12,10 @@ urlpatterns = [
     path('book-add/', views.book_add, name='book_add'),
     path('book-edit/<int:book_id>/', views.book_edit, name='book_edit'),
     path('book-delete/<int:book_id>/', views.book_delete, name='book_delete'),
+
+    path('profile/', views.user_profile, name='user_profile'),
+    path('add-to-cart/<int:book_id>/', views.add_to_cart, name='add_to_cart'),
+    path('remove-from-cart/<int:book_id>/', views.remove_from_cart, name='remove_from_cart'),
+
+    path('cart/', views.view_cart, name='cart'),
 ]
